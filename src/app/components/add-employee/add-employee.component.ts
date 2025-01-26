b1078af6e328c3155bac9fe9a9043fd1abbfa274
Muhammadd-01 <muhammadaffan1445@gmail.com>
2025-01-26 16:47:55 +0500

    Angular

--- src/app/components/add-employee/add-employee.component.ts
+++ src/app/components/add-employee/add-employee.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-add-employee',
   imports: [],
-  template: '<h1 class="primary">add-employee works!</h1>',
+  template: '<h1 class="text-primary">add-employee works!</h1>',
   styles: '[.primary{background-color: blue}]',
 })
 export class AddEmployeeComponent {
